0e133171db53837bf1449419fda0494bda2d2427
RezHackXYZ <rhythmupa@gmail.com>
2025-06-24 10:43:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cube timer
+a wip cube timer
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import './app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="bg-white h-full font-[Space_Grotesk]">{@render children()}</div>
+
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300&display=swap");
+</style>
